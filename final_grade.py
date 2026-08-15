1495f0d8bbd30d08ba9a8cd40fe065644a38021c
Juvelism <jborrel01@gmail.com>
2026-08-15 10:23:48 +0800

    grades

--- final_grade.py
+++ final_grade.py
@@ -1,2 +1,12 @@
 def final_grade(exam, projects):
-    return # final grade
+
+    if exam > 90 or projects > 10:
+        print(100)
+    elif exam > 75 and projects >= 5:
+        print(90)
+    elif exam > 50 and projects >= 2:
+        print(75)
+    else:
+        print(0)
+
+final_grade(55,0)
