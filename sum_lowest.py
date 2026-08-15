3b2abaa2dfa8909e7ae4979a8a63ab4a54d3e359
wbxpzeroone <wbxpzeroone@proton.me>
2026-08-15 19:22:53 +0800

    rank up to kata 6

--- sum_lowest.py
+++ sum_lowest.py
@@ -1,4 +1,11 @@
 def sum_two_smallest_numbers(numbers):
 
-    
-    return 0
+    num1 = min(numbers)
+    numbers.remove(num1)
+    num2 = min(numbers)
+
+    print(num1 + num2)
+
+
+    print(type(num2))
+sum_two_smallest_numbers([19, 5, 42, 2, 77])
