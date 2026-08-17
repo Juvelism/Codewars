db33da2ed022de4330624feac5a5a420a8d365a7
Juvelism <jborrel01@gmail.com>
2026-08-17 09:42:08 +0800

    testing

--- mini_vending_machine.py
+++ mini_vending_machine.py
@@ -3,3 +3,14 @@ coffee_menu = [
     {"name": "Latte", "price": 70, "stock": 3},
     {"name": "Cappuccino", "price": 80, "stock": 2}
 ]
+
+
+print("=== COFFEE MENU ===\n")
+
+for menu in coffee_menu:
+    print(f'{menu["name"]} - {menu["price"]}')
+
+coffee_name = input("\nChoose Coffee: ")
+
+print(f'You selected: {coffee_name.title()}')
+print(f'Price: P{coffee_menu["price"]}')
