a1a9e0b3d984850e5e6d853ce0e0531ee560c121
Juvs <jborrel01@gmail.com>
2026-08-18 12:44:58 +0800

    upgrade

--- mini_vending_machine.py
+++ mini_vending_machine.py
@@ -1,20 +1,18 @@
 coffee_menu = [
     {"name": "Espresso", "price": 50, "stock": 5},
-    {"name": "Latte", "price": 70, "stock": 3},
+    {"name": "Latte", "price": 70, "stock": 0},
     {"name": "Cappuccino", "price": 80, "stock": 2}
 ]
 
 
 print("=== COFFEE MENU ===\n")
 
-# VIEW OF MENU
+# DISPLAY COFFEE MENU
 for menu in coffee_menu:
     print(f'{menu["name"]} - {menu["price"]}')
 
 customer_choice = input("\nChoose Coffee: ").title()
-
 print(f"\nYou selected: {customer_choice}")
 
-for price in coffee_menu:
-    if price['name'] == customer_choice:
-        print(f"Price: {price['price']}")
+for coffee in coffee_menu:
+    print(coffee)
